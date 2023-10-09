8cc4923a9c202bdd05012144de2e613f790aa80a
dtrensuri <dothanhcao2502@gmail.com>
2023-10-09 16:40:00 +0700

    update export excel

--- app/controller/sheet/attendanceSummarySheet.ts
+++ app/controller/sheet/attendanceSummarySheet.ts
@@ -13,192 +13,7 @@ const ngayLe = [
   { day: "2", month: "9", title: "Quốc Khánh" },
 ];
 
-const attendanceSummarySheet = async (
-  data: AttendanceSummarySheet,
-  fileName: string
-) => {
-  const workbook = new ExcelJS.Workbook();
-  const worksheet = workbook.addWorksheet("Bảng Giờ Công");
-  worksheet.font = {
-    name: "Arial",
-    size: 10,
-  };
-
-  const numCol = 16;
-  const row1 = worksheet.addRow(["Đơn vị: DTC Software"]);
-  const row2 = worksheet.addRow(["Địa chỉ: "]);
-  const title = worksheet.addRow(["TỔNG HỢP CHẤM CÔNG"]);
-  const timeTitle = worksheet.addRow(["Từ ngày  đến ngày"]);
-  const row5 = worksheet.addRow([]);
-  const header = worksheet.addRow([
-    "STT",
-    "Mã nhân viên",
-    "Tên nhân viên",
-    "Phòng ban",
-    "Chức vụ",
-    "Ngày vào làm",
-    "Tổng công/Tháng",
-    "Ngày công",
-    "",
-    "",
-    "Giờ công",
-    "",
-    "",
-    "Vào trễ",
-    "",
-    "Ra sớm",
-    "",
-    "Tăng ca",
-    "",
-    "",
-    "Vắng KP",
-    "Ngày nghỉ",
-  ]);
-  const headerChildren = worksheet.addRow([
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "Thường",
-    "C.Tuần",
-    "Q.Đêm",
-    "Thường",
-    "C.Tuần",
-    "Q.Đêm",
-    "Lần",
-    "Phút",
-    "Lần",
-    "Phút",
-    "TC1",
-    "TC2",
-    "TC3",
-    "",
-    "OM",
-    "TS",
-    "R",
-    "Ro",
-    "P",
-    "F",
-    "CO",
-    "CD",
-    "H",
-    "CT",
-    "Le",
-  ]);
-
-  worksheet.mergeCells("A1:AF1");
-  worksheet.mergeCells("A2:AF2");
-  worksheet.mergeCells("A3:AF3");
-  worksheet.mergeCells("A4:AF4");
-  worksheet.mergeCells("A6:A7");
-  worksheet.mergeCells("B6:B7");
-  worksheet.mergeCells("C6:C7");
-  worksheet.mergeCells("D6:D7");
-  worksheet.mergeCells("E6:E7");
-  worksheet.mergeCells("F6:F7");
-  worksheet.mergeCells("G6:G7");
-  worksheet.mergeCells("H6:J6");
-  worksheet.mergeCells("K6:M6");
-  worksheet.mergeCells("N6:O6");
-  worksheet.mergeCells("P6:Q6");
-  worksheet.mergeCells("R6:T6");
-  worksheet.mergeCells("V6:AF6");
-  worksheet.mergeCells("U6:U7");
-
-  headerChildren.height = 40;
-  const table = worksheet.addTable({
-    name: "MyTable",
-    ref: "A8",
-    headerRow: false,
-    totalsRow: false,
-    style: {
-      showRowStripes: true,
-      border: {
-        top: { style: "thin", color: { argb: "000000" } },
-        left: { style: "thin", color: { argb: "000000" } },
-        right: { style: "thin", color: { argb: "000000" } },
-        bottom: { style: "thin", color: { argb: "000000" } },
-      },
-    },
-    columns: [
-      { name: "A" },
-      { name: "B" },
-      { name: "C" },
-      { name: "D" },
-      { name: "E" },
-      { name: "F" },
-      { name: "G" },
-      { name: "H" },
-      { name: "I" },
-      { name: "J" },
-      { name: "K" },
-      { name: "L" },
-      { name: "M" },
-      { name: "N" },
-      { name: "O" },
-      { name: "P" },
-      { name: "Q" },
-      { name: "R" },
-      { name: "S" },
-      { name: "T" },
-      { name: "U" },
-      { name: "V" },
-      { name: "W" },
-      { name: "X" },
-      { name: "Y" },
-      { name: "Z" },
-      { name: "AA" },
-      { name: "AB" },
-      { name: "AC" },
-      { name: "AD" },
-      { name: "AE" },
-      { name: "AF" },
-    ],
-    rows: [],
-  });
-  title.eachCell((cell: any) => {
-    cell.style = {
-      alignment: { vertical: "middle", horizontal: "center", wrapText: true },
-      font: {
-        size: 14,
-        bold: true,
-      },
-    };
-  });
-
-  const headerRow = [headerChildren, header];
-  _.forEach(headerRow, (row: any) => {
-    row.eachCell((cell: any) => {
-      cell.style = {
-        font: {
-          size: 10,
-          bold: true,
-        },
-        alignment: { vertical: "middle", horizontal: "center", wrapText: true },
-        border: {
-          top: { style: "thin", color: { argb: "000000" } },
-          left: { style: "thin", color: { argb: "000000" } },
-          right: { style: "thin", color: { argb: "000000" } },
-          bottom: { style: "thin", color: { argb: "000000" } },
-        },
-      };
-    });
-  });
-
-  const buf = await workbook.xlsx.writeBuffer();
-  const blob = new Blob([buf], {
-    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-  });
-  saveAs(blob, `${fileName}.xlsx`);
-};
-
-const timeSheetDetail = async (
-  data: AttendanceSummarySheet,
-  fileName: string
-) => {
+const timeSheetDetail = async (data: DataTimeSheetDetail, fileName: string) => {
   const workbook = new ExcelJS.Workbook();
   workbook.calcProperties.fullCalcOnLoad = true;
   const worksheet = workbook.addWorksheet("Bảng chi tiết chấm công");
@@ -278,6 +93,7 @@ const timeSheetDetail = async (
   title[6] = worksheet.addRow(["", "Các loại vắng"]);
   title[7] = worksheet.addRow([
     "",
+    "",
     "V",
     "OM",
     "TS",
@@ -291,23 +107,22 @@ const timeSheetDetail = async (
     "CT",
     "Le",
     "",
-    "",
   ]);
   title[8] = worksheet.addRow([
     "",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
-    "0",
     "",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
     "",
   ]);
   title[9] = worksheet.addRow(["Chi tiết"]);
@@ -441,11 +256,14 @@ const timeSheetDetail = async (
       "",
       "",
       "",
-      value.late,
-      value.early,
-      value.work_time,
-      value.in_office,
-      value.ot,
+      value.late ? value.late : 0,
+      value.early ? value.early : 0,
+      value.work_time ? value.work_time : 0,
+      value.in_office ? value.in_office : 0,
+      value.ot ? value.ot : 0,
+      0,
+      0,
+      "",
     ]);
     rowsData.push(row);
   });
@@ -458,7 +276,7 @@ const timeSheetDetail = async (
   worksheet.getCell("D4").value = cuoiTuan * 8;
   worksheet.getCell("D5").value = { formula: "D3+D4" };
 
-  worksheet.getCell("H3").value = ot;
+  worksheet.getCell("H3").value = (ot / 60).toFixed(1);
   worksheet.getCell("L4").value = slDiTre;
   worksheet.getCell("L5").value = spDiTre;
   worksheet.getCell("P4").value = slVeSom;
@@ -505,6 +323,17 @@ const timeSheetDetail = async (
 
   _.forEach(rowsData, (row: any, id: any) => {
     if (row) {
+      for (let i = 0; i < numCol; i++) {
+        row.getCell(i + 1).font = {
+          italic: true,
+        };
+        row.getCell(i + 1).border = {
+          top: { style: "dotted" },
+          left: { style: "thin" },
+          right: { style: "thin" },
+        };
+      }
+
       row.alignment = {
         vertical: "middle",
         horizontal: "center",
@@ -524,7 +353,15 @@ const timeSheetDetail = async (
       }
     }
   });
-
+  const lastRowData = _.last(rowsData);
+  for (let i = 0; i < numCol; i++) {
+    lastRowData.getCell(i + 1).border = {
+      top: { style: "dotted" },
+      left: { style: "thin" },
+      right: { style: "thin" },
+      bottom: { style: "thin" },
+    };
+  }
   const buf = await workbook.xlsx.writeBuffer();
   const blob = new Blob([buf], {
     type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
@@ -533,6 +370,5 @@ const timeSheetDetail = async (
 };
 
 module.exports = {
-  attendanceSummarySheet,
   timeSheetDetail,
 };
